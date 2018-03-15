4b531d0c0984585314cfd33b8b3ab6b2a7b2dae0
Ryan <rbrigby@gmail.com>
2018-03-15 13:06:55 -0600

    Update application.js

--- js/application.js
+++ js/application.js
@@ -1,24 +1,7 @@
 (function() {
 
   $(document).ready(function() {
-    $('#github').mouseover(function() {
-      return $('#mousing').text("GitHub");
-    });
-    $('#github').mouseout(function() {
-      return $('#mousing').html("&nbsp;");
-    });
-    $('#twitter').mouseover(function() {
-      return $('#mousing').text("Twitter");
-    });
-    $('#twitter').mouseout(function() {
-      return $('#mousing').html("&nbsp;");
-    });
-    $('#rss').mouseover(function() {
-      return $('#mousing').text("RSS");
-    });
-    return $('#rss').mouseout(function() {
-      return $('#mousing').html("&nbsp;");
-    });
+  
   });
 
 }).call(this);
